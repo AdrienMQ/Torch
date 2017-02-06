5e26125c3a45367c364dce2fadd9f612fffe891b
Adrien Martin <adrienmartin.1991@gmail.com>
2017-02-06 23:43:09 +1100

    [DEV] Change click behaviour and add new mode ECO
    Blink when blink clicked.
    Light when torch clicked.
    Eco when eco clicked.
    
    Persisting secondary bug : if multiple click on blink, multiple threads may be created and light template  will not be assured.

--- app/src/main/java/com/skayne/torch/MainActivity.java
+++ app/src/main/java/com/skayne/torch/MainActivity.java
@@ -1,7 +1,5 @@
 package com.skayne.torch;
 
-//Classic import
-
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
@@ -12,117 +10,172 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.RequiresApi;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
-//More specific import
-
+import static android.view.View.VISIBLE;
 
 @RequiresApi(api = Build.VERSION_CODES.M)
+
 public class MainActivity extends Activity {
+    private static final int REF_TORCHLIGHT = 1;
+    private static final int REF_BLINKLIGHT = 2;
+    private static final int REF_ECOLIGHT = 3;
+    public Handler handler;
+    protected TextView tvState;
     Context context = this;
     boolean bBlink = false;
     boolean bTorch = false;
-    boolean hasFlash;
-
-    private TextView textView;
-    private TextView notSupported;
-    private Button btTorch;
-    private Button btBlink;
-    private Handler handler;
-
+    boolean hasFlash = false;
+    private ToggleButton tbTorch;
+    private ToggleButton tbBlink;
+    private ToggleButton tbEco;
+    private Thread torchThread;
+    private int lightState = 0;
+    private int mode = 2; //TODO: Add blink mode 'quick' or 'slow'
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        textView = (TextView) findViewById(R.id.textLight);
-        btTorch = (Button) findViewById(R.id.toggleButton);
-        btBlink = (Button) findViewById(R.id.blinkButton);
-        notSupported = (TextView) findViewById(R.id.notSupported);
-        textView.setVisibility(View.INVISIBLE);
-        textView.setVisibility(View.INVISIBLE);
-
+        tbTorch = (ToggleButton) findViewById(R.id.torchButton);
+        tbBlink = (ToggleButton) findViewById(R.id.blinkButton);
+        tbEco = (ToggleButton) findViewById(R.id.ecoButton);
+        tvState = (TextView) findViewById(R.id.textLight);
+        tvState.setVisibility(View.INVISIBLE);
         handler = new Handler();
 
+        //Check flash is supported
+        TextView tvSupported = (TextView) findViewById(R.id.textSupported);
+        tvSupported.setVisibility(View.INVISIBLE);
         hasFlash = context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
         if (!hasFlash) {
             Toast.makeText(this, "The device has no integrated flash.", Toast.LENGTH_LONG).show();
-            textView.setVisibility(View.VISIBLE);
+            tvSupported.setVisibility(VISIBLE);
         }
     }
 
-    public void changeLightState(View view) {
+    public void onTorch(View view) {
         boolean checked = ((ToggleButton) view).isChecked();
-        Thread torchThread = new Thread(new TorchManagerThread());
 
-        if (hasFlash && checked) {
-            textView.setText(R.string.text_on);
-            textView.setVisibility(View.VISIBLE);
-            bTorch = true;
+        if (lightState != REF_TORCHLIGHT) tbTurnOff(lightState);  //Toggle actual mode
+        lightState = 0;
 
-            //Start thread
-            torchThread.start();
-        } else if (hasFlash) {
-            textView.setText(R.string.text_off);
-            bTorch = false;
-        }
+        tbTurnOff(lightState);  //Toggle actual mode
+        if (checked)             //Turn ON new mode if exists
+        {
+            lightState = REF_TORCHLIGHT;
+            tvState.setText(R.string.text_on);
+            tvState.setVisibility(View.VISIBLE);
+
+            new Thread(new TorchManagerThread()).start();
+        } else tvState.setText(R.string.text_off);
     }
 
-    public void changeLightMode(View view) {
-        bBlink = ((ToggleButton) view).isChecked();
+    public void onBlink(View view) {
+        boolean checked = ((ToggleButton) view).isChecked();
+
+        if (lightState != REF_BLINKLIGHT) tbTurnOff(lightState);  //Toggle actual mode
+        lightState = 0;
+
+        tbTurnOff(lightState);  //Toggle actual mode
+        if (checked)             //Turn ON new mode if exists
+        {
+            lightState = REF_BLINKLIGHT;
+            tvState.setText(R.string.text_blink);
+            tvState.setVisibility(View.VISIBLE);
+
+            new Thread(new TorchManagerThread()).start();
+        } else tvState.setText(R.string.text_off);
+    }
+
+    public void onEco(View view) {
+        boolean checked = ((ToggleButton) view).isChecked();
+
+        if (lightState != REF_ECOLIGHT) tbTurnOff(lightState);  //Toggle actual mode
+        lightState = 0;
+
+        if (checked)             //Turn ON new mode if exists
+        {
+            lightState = REF_ECOLIGHT;
+            tvState.setText(R.string.text_eco);
+            tvState.setVisibility(View.VISIBLE);
+
+            new Thread(new TorchManagerThread()).start();
+        } else tvState.setText(R.string.text_off);
+    }
+
+    private void tbTurnOff(int lightState) {
+        switch (lightState) {
+            case REF_TORCHLIGHT:
+                tbTorch.toggle();
+                break;
+            case REF_BLINKLIGHT:
+                tbBlink.toggle();
+                break;
+            case REF_ECOLIGHT:
+                tbEco.toggle();
+                break;
+        }
     }
 
     private class TorchManagerThread implements Runnable {
-
         private CameraManager manager;
 
         @Override
         public void run() {
+            final int state = lightState;
             manager = (CameraManager) context.getSystemService(Context.CAMERA_SERVICE);
             try {
-                while (bTorch) {
+                if (!hasFlash || state == 0) return;
 
+                //End the thread when variable 'lightState' changes
+                while (state == lightState) {
+                    //TODO: New strategy to avoid double click bug (2 or more threads created)
                     manager.setTorchMode(manager.getCameraIdList()[0], true);
-                    if (bBlink) {
-                        //Flashlight process
-                        // Mode 1 : |_    __________               | (1.420s)
-                        /*
-                        manager.setTorchMode(manager.getCameraIdList()[0], true);
-                        Thread.sleep(70);
-                        manager.setTorchMode(manager.getCameraIdList()[0], false);
-                        Thread.sleep(150);
-                        manager.setTorchMode(manager.getCameraIdList()[0], true);
-                        Thread.sleep(500);
-                        manager.setTorchMode(manager.getCameraIdList()[0], false);
-                        Thread.sleep(200);
-                        */
 
-                        // Mode 2 : |_   ___     __________        | (1.770s)
-                        manager.setTorchMode(manager.getCameraIdList()[0], true);
-                        Thread.sleep(70);
-                        manager.setTorchMode(manager.getCameraIdList()[0], false);
-                        Thread.sleep(150);
-                        manager.setTorchMode(manager.getCameraIdList()[0], true);
-                        Thread.sleep(100);
-                        manager.setTorchMode(manager.getCameraIdList()[0], false);
-                        Thread.sleep(250);
-                        manager.setTorchMode(manager.getCameraIdList()[0], true);
-                        Thread.sleep(500);
+                    if (lightState == REF_BLINKLIGHT) {
+                        //Flashlight process
+
+                        // Mode blink 1 : |_    __________               | (1.420s)
+                        if (mode == 1) {
+                            Thread.sleep(70);
+                            manager.setTorchMode(manager.getCameraIdList()[0], false);
+                            Thread.sleep(150);
+                            manager.setTorchMode(manager.getCameraIdList()[0], true);
+                            Thread.sleep(500);
+                            manager.setTorchMode(manager.getCameraIdList()[0], false);
+                            Thread.sleep(200);
+                        }
+
+                        // Mode blink 2 : |_   ___     __________        | (1.770s)
+                        if (mode == 2) {
+                            Thread.sleep(70);
+                            manager.setTorchMode(manager.getCameraIdList()[0], false);
+                            Thread.sleep(150);
+                            manager.setTorchMode(manager.getCameraIdList()[0], true);
+                            Thread.sleep(100);
+                            manager.setTorchMode(manager.getCameraIdList()[0], false);
+                            Thread.sleep(250);
+                            manager.setTorchMode(manager.getCameraIdList()[0], true);
+                            Thread.sleep(500);
+                            manager.setTorchMode(manager.getCameraIdList()[0], false);
+                            Thread.sleep(200);
+                        }
+                    } else if (lightState == REF_ECOLIGHT) {
+                        // Mode blink eco : |________           | (1.000s)
+                        Thread.sleep(300);
                         manager.setTorchMode(manager.getCameraIdList()[0], false);
                         Thread.sleep(200);
                     }
-
                     Thread.sleep(500);
                 }
+
+                //End thread turning OFF
                 manager.setTorchMode(manager.getCameraIdList()[0], false);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (CameraAccessException e) {
+            } catch (InterruptedException | CameraAccessException e) {
                 e.printStackTrace();
             }
         }
